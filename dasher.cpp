42284bcea571c7f141d9cbe201f32bc9c74fa711
Bartosz Wlazlowski <43954519+Manchuskoi@users.noreply.github.com>
2024-05-01 13:58:59 +0200

    Creating movement for Dasher Sprite and adding Enemy

--- dasher.cpp
+++ dasher.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    // setting target fps
+    //setting target fps
     SetTargetFPS(60);
 
     //window dimension
@@ -11,9 +11,14 @@ int main()
     //initialize the window 
     InitWindow(windowWidth, windowHeight, "My Game Window");
 
-    //acceleration due to gravity (pixels/frame)/frame
-    const int gravity{1};
+    //acceleration due to gravity (pixels/s)/s
+    const int gravity{1'000};
 
+
+    //nebula variables 
+    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
+
+    //scarfy variables
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
     Rectangle scarfyRec;
     scarfyRec.width = scarfy.width/6;
@@ -24,10 +29,18 @@ int main()
     scarfyPos.x = windowWidth / 2 - scarfyRec.width / 2; 
     scarfyPos.y = windowHeight - scarfyRec.height; 
 
+    //animation frame
+    int frame{}; // starts with 0 
+    //amount of time before we update the animation frame
+    const float updateTime{1.0/12.0}; 
+    //running Time how much time passed since last update of animation 
+    float runningTime{};
+    
+
     //boolean check if rectangle is in air
     bool isInAir = false;
-    //jump velocity
-    const int jumpVel{-22};
+    //jump velocity (pixels/s)
+    const int jumpVel{-600};
 
 
     int velocity{0};
@@ -38,6 +51,9 @@ int main()
         //start drawing 
         BeginDrawing();
         ClearBackground(WHITE);
+
+        //using delta time (time since last frame)
+        const float dT {GetFrameTime()};
 
         
         //perform ground check
@@ -51,7 +67,7 @@ int main()
         {
             //rectangle in in the air 
             //apply gravity
-            velocity += gravity;
+            velocity += gravity * dT;
             isInAir = true;
         }
 
@@ -63,7 +79,22 @@ int main()
 
 
         //update Y position
-        scarfyPos.y += velocity;
+        scarfyPos.y += velocity * dT;
+        //update running time 
+        runningTime += dT; 
+        if (runningTime >= updateTime)
+        {
+            runningTime = 0.0;
+            //update animation frame 
+            scarfyRec.x = frame * scarfyRec.width; 
+            frame++;
+            if (frame > 5)
+            {
+                frame = 0;  
+            }
+        }
+
+        
 
         DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE); 
 
@@ -71,5 +102,6 @@ int main()
         EndDrawing();
     }
     UnloadTexture(scarfy); 
+    UnloadTexture(nebula);
     CloseWindow();
 }
